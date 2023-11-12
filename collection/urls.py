d15616f93415949767dea6285ab642a41420b7f3
Alejandro-Fierro <luis.fierro193@tectijuana.edu.mx>
2023-11-12 15:38:30 -0800

    pagina de pinturas por artista

--- collection/urls.py
+++ collection/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("artworks/<int:artwork_id>/", artworks.getArtwork, name="artwork"),
     path("store/", store.getArtworksPackages, name="store"),
     path("artists/",artists.getArtistsList,name="artist"),
+    path("artists/<int:artist_id>/",artists.getArtist,name="artbyauthor"),
     path("api/store/create-payment/", store.getStripeClientSecret, name="client_secret"),
     path("api/store/package", store.saveArtPackageToUser, name="save_package"),
     path("search/", artworks.ArtWorkSearchView.as_view(),name="art-list"),
--- collection/views/artists.py
+++ collection/views/artists.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from collection.models import Artist
+from collection.models import Artist,Artwork
 
 def getArtistsList(request):
     artists = Artist.objects.all()
@@ -9,5 +9,7 @@ def getArtistsList(request):
 
 def getArtist(request, artist_id):
     artist = Artist.objects.get(pk=artist_id)
+    arts = Artwork.objects.filter(author=artist)
+    return render(request, "artworks/index.html", {"artworks": arts})
 
-    return render(request, "artists/artists.html", {"artist": artist})
+

6183d157ac3cf031450638ac780942744f58c0e9
Jordi <jordi8101@gmail.com>
2023-11-11 18:48:32 -0800

    feat: update profile

--- collection/views/profile.py
+++ collection/views/profile.py
@@ -1,17 +1,23 @@
 from collection.models import PackageOrder
 from django.shortcuts import render
+from collections import defaultdict
 
 
 def getMyArtworks(request):
     artworksPackage = PackageOrder.objects.filter(user=request.user)
 
-    artworks = []
+    artworks = defaultdict(int)
 
     for package in artworksPackage:
-        artworks.extend(package.artworks.all())
-
-    artworks = list(set(artworks))
+        for artwork in package.artworks.all():
+            print(artwork.title)
+            key = (artwork.title, artwork.image_url, artwork.id)
+            artworks[key] += 1
 
     return render(
-        request, "profile/index.html", {"artworks": artworks, "quantity": len(artworks)}
+        request,
+        "profile/index.html",
+        {
+            "artworks": dict(artworks),
+        },
     )

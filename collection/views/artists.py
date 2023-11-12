50b2bef8b0f961668127c8f2d98a1a791586a2d5
Jordi <jordi8101@gmail.com>
2023-11-12 15:44:20 -0800

    fix: added empty screen for artists and added artists to header

--- collection/views/artists.py
+++ collection/views/artists.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from collection.models import Artist,Artwork
+from collection.models import Artist, Artwork
+
 
 def getArtistsList(request):
     artists = Artist.objects.all()
@@ -10,6 +11,10 @@ def getArtistsList(request):
 def getArtist(request, artist_id):
     artist = Artist.objects.get(pk=artist_id)
     arts = Artwork.objects.filter(author=artist)
+
+    if arts.count() == 0:
+        return render(
+            request, "404.html", {"message": "No artworks found for this artist"}
+        )
+
     return render(request, "artworks/index.html", {"artworks": arts})
-
-
